feb3971769959229a7c55f9a7eba18b7a13755db
saadabdullah1420-oss <saadabdullah1420@gmail.com>
2026-08-18 19:20:24 +0300

    Add binary tree nodes function

--- binary_trees.h
+++ binary_trees.h
@@ -55,4 +55,7 @@ size_t binary_tree_size(const binary_tree_t *tree);
 /* T12 prototype */
 size_t binary_tree_leaves(const binary_tree_t *tree);
 
+/* T13 prototype */
+size_t binary_tree_nodes(const binary_tree_t *tree);
+
 #endif /* BINARY_TREES_H */
